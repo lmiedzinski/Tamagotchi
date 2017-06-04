a160be4df1bd34718c938679859584247648f7c0
Łukasz Miedziński <luxorek@gmail.com>
2017-06-04 16:17:38 +0200

    [#5] Dodane klasy widoku dla użytkowników

---
+++ src/main/java/pl/dbjllmjk/View/LoginView.java
@@ -0,0 +1,147 @@
+package pl.dbjllmjk.View;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+
+import pl.dbjllmjk.Controller.LoginController;
+import pl.dbjllmjk.Model.BadPasswordException;
+import pl.dbjllmjk.Model.NoSuchUserException;
+
+public class LoginView extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	private JTextField loginField;
+	private JPasswordField passField;
+	private JButton loginButton;
+	private JButton exitButton;
+	private JLabel loginLabel;
+	private LoginController loginController;
+	private JLabel passwordLabel;
+	private JButton registrationButton;
+	private JButton authorsButton;
+
+	public LoginView(LoginController loginController) throws HeadlessException {
+		super("Tamagotchi - DBJLLMJK");
+		this.loginController = loginController;
+		this.setResizable(false);
+		init();
+		this.setSize(500, 800);
+		this.setLocationRelativeTo(null);
+		this.setLayout(new GridLayout(0, 1));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == exitButton)
+			System.exit(0);
+		if (arg0.getSource() == registrationButton) {
+			String[] possibilities = { "user" };
+			JComboBox<String> combo = new JComboBox<String>(possibilities);
+			JTextField login = new JTextField("");
+			JTextField password = new JTextField("");
+			JTextField name = new JTextField("");
+			JTextField surname = new JTextField("");
+			JPanel userForm = new JPanel(new GridLayout(0, 1));
+			userForm.add(new JLabel("Account type:"));
+			userForm.add(combo);
+			userForm.add(new JLabel("Login:"));
+			userForm.add(login);
+			userForm.add(new JLabel("Password:"));
+			userForm.add(password);
+			userForm.add(new JLabel("Name:"));
+			userForm.add(name);
+			userForm.add(new JLabel("Surname:"));
+			userForm.add(surname);
+			int result = JOptionPane.showConfirmDialog(null, userForm, "Create new account",
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (result == JOptionPane.OK_OPTION) {
+
+				try {
+					if (combo.getSelectedItem() == "user")
+						this.loginController.addAccount(login.getText(), password.getText(), name.getText(),
+								surname.getText());
+				} catch (NoSuchUserException e) {
+					JOptionPane.showMessageDialog(this, e.getMessage() + "\n( ͡° ͜ʖ ͡°)", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		}
+		if (arg0.getSource() == loginButton) {
+			try {
+				loginController.tryToLog(this.loginField.getText(), new String(this.passField.getPassword()));
+				this.setVisible(false);
+				dispose();
+			} catch (NoSuchUserException e) {
+				JOptionPane.showMessageDialog(this, "No such user!\n( ͡° ͜ʖ ͡°)", "Warning",
+						JOptionPane.WARNING_MESSAGE);
+			} catch (BadPasswordException e) {
+				JOptionPane.showMessageDialog(this, "Bad password!\n( ͡° ͜ʖ ͡°)", "Warning",
+						JOptionPane.WARNING_MESSAGE);
+			}
+		}
+		if (arg0.getSource() == authorsButton){
+			JPanel authorsPanel = new JPanel(new GridLayout(0, 1));
+			authorsPanel.add(new JLabel("Authors:"));
+			authorsPanel.add(new JLabel("Jacek Lewański 195643"));
+			authorsPanel.add(new JLabel("Łukasz Miedziński 195659"));
+			authorsPanel.add(new JLabel("Damian Bartosik 195559"));
+			authorsPanel.add(new JLabel("Jakub Krysiak 195634"));
+			authorsPanel.add(new JLabel("( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)"));
+			JOptionPane.showConfirmDialog(null, authorsPanel, "Authors",
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		}
+	}
+
+	private void init() {
+		this.loginField = new JTextField("", 13);
+		this.passField = new JPasswordField("", 13);
+		this.loginButton = new JButton("Login");
+		this.exitButton = new JButton("Exit");
+		this.loginLabel = new JLabel("  Login: ");
+		this.passwordLabel = new JLabel("  Password:");
+		this.registrationButton = new JButton("Register");
+		this.authorsButton = new JButton("Authors");
+		this.authorsButton.addActionListener(this);
+		JPanel up = new JPanel(new GridLayout(0, 1));
+		JPanel down = new JPanel(new GridLayout(0, 4));
+		up.add(this.loginLabel);
+		up.add(this.loginField);
+		up.add(this.passwordLabel);
+		up.add(this.passField);
+		down.add(this.loginButton);
+		down.add(this.registrationButton);
+		down.add(this.authorsButton);
+		down.add(this.exitButton);
+		add(up);
+		add(down);
+		this.loginButton.addActionListener(this);
+		this.exitButton.addActionListener(this);
+		this.registrationButton.addActionListener(this);
+		this.passField.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					loginButton.doClick();
+				}
+			}
+		});
+	}
+}
---
+++ src/main/java/pl/dbjllmjk/View/UserView.java
@@ -0,0 +1,188 @@
+package pl.dbjllmjk.View;
+
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import pl.dbjllmjk.Controller.UserController;
+import pl.dbjllmjk.Model.PetTransactionException;
+
+public class UserView extends JFrame implements WindowListener, ActionListener {
+	private static final long serialVersionUID = 1L;
+	private UserController userController;
+	private JTabbedPane tabbedPane;
+	private UserPet petTab;
+	private UserMenu menuTab;
+	private JMenuItem menuItem1, menuItem2;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public UserPet getPetTab() {
+		return petTab;
+	}
+
+	public UserMenu getMenuTab() {
+		return menuTab;
+	}
+
+	public UserView(UserController userController) throws HeadlessException {
+		super("User Panel: " + userController.getLoggedUser().getLogin() + " - Tamagotchi DBJLLMJK");
+		this.userController = userController;
+		this.setResizable(false);
+		this.addWindowListener(this);
+		init();
+		this.setLocation(200, 200);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.setSize(new Dimension(700, 500));
+		setVisible(true);
+	}
+
+	private void init() {
+		JMenuBar menuBar;
+		JMenu menu;
+		menuBar = new JMenuBar();
+		menu = new JMenu("File");
+		menuItem1 = new JMenuItem("Import pet...");
+		menuItem2 = new JMenuItem("Export pet...");
+		menuItem1.addActionListener(this);
+		menuItem2.addActionListener(this);
+		menu.add(menuItem1);
+		menu.add(menuItem2);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		this.tabbedPane = new JTabbedPane();
+		petTab = new UserPet(this.userController);
+		menuTab = new UserMenu(this.userController, petTab);
+		this.tabbedPane.addTab("Menu", menuTab);
+		this.tabbedPane.addTab("Pet", petTab);
+		add(this.tabbedPane);
+	}
+
+	public void windowClosing(WindowEvent e) {
+		this.userController.logout();
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == this.menuItem1) {
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setDialogTitle("Select file with your pet");
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.setMultiSelectionEnabled(false);
+			fileChooser.setFileFilter(new FileNameExtensionFilter("XML File", "xml"));
+			int responce = fileChooser.showOpenDialog(this);
+			if (responce == JFileChooser.APPROVE_OPTION) {
+				try {
+					this.userController.importPet(fileChooser.getSelectedFile().getAbsolutePath(), false);
+					JOptionPane.showMessageDialog(this, "Success! Pet imported!\n( ͡° ͜ʖ ͡°)", "Success",
+							JOptionPane.INFORMATION_MESSAGE);
+					this.userController.refresh();
+				} catch (PetTransactionException e) {
+					if (e.getMessage().contains("You already have pet")) {
+						int option = JOptionPane.showConfirmDialog(null,
+								e.getMessage() + "\nDo you want to replace current pet with new?", "Pet already exist",
+								JOptionPane.YES_NO_OPTION);
+						if (option == JOptionPane.YES_OPTION) {
+							try {
+								this.userController.importPet(fileChooser.getSelectedFile().getAbsolutePath(), true);
+								JOptionPane.showMessageDialog(this, "Success! Pet imported!\n( ͡° ͜ʖ ͡°)", "Success",
+										JOptionPane.INFORMATION_MESSAGE);
+								this.userController.refresh();
+							} catch (PetTransactionException e1) {
+								JOptionPane.showMessageDialog(this, e.getMessage() + "\n( ͡° ͜ʖ ͡°)", "Error",
+										JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					} else {
+						JOptionPane.showMessageDialog(this, e.getMessage() + "\n( ͡° ͜ʖ ͡°)", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			} else if (responce == JFileChooser.CANCEL_OPTION) {
+				// do nothing
+			} else {
+				JOptionPane.showMessageDialog(this, "Error opening selected file!\n( ͡° ͜ʖ ͡°)", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		if (arg0.getSource() == this.menuItem2) {
+			if (this.menuTab.getPetlist().getSelectedValue() == null) {
+				JOptionPane.showMessageDialog(this, "No pet selected!!!\n( ͡° ͜ʖ ͡°)", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			} else {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Select file with your pet");
+				fileChooser.setAcceptAllFileFilterUsed(false);
+				fileChooser.setMultiSelectionEnabled(false);
+				fileChooser.setFileFilter(new FileNameExtensionFilter("XML File", "xml"));
+				fileChooser.setSelectedFile(new File(this.menuTab.getPetlist().getSelectedValue().getTitle()));
+				int responce = fileChooser.showSaveDialog(this);
+				if (responce == JFileChooser.APPROVE_OPTION) {
+					try {
+						this.userController.exportPet(fileChooser.getSelectedFile().getAbsolutePath(),
+								fileChooser.getSelectedFile().getName(),
+								this.menuTab.getPetlist().getSelectedValue().getTitle());
+						int option = JOptionPane.showConfirmDialog(null,
+								"Pet saved!\nDo you want to remove saved pet from your account?", "Remove pet",
+								JOptionPane.YES_NO_OPTION);
+						if (option == JOptionPane.YES_OPTION) {
+							this.userController.deletePet(this.menuTab.getPetlist().getSelectedValue().getTitle());
+							JOptionPane.showMessageDialog(this, "Saved pet removed from your account!\n( ͡° ͜ʖ ͡°)", "Success",
+									JOptionPane.INFORMATION_MESSAGE);
+							this.userController.refresh();
+						}
+					} catch (PetTransactionException e) {
+						JOptionPane.showMessageDialog(this, e.getMessage() + "\n( ͡° ͜ʖ ͡°)", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		}
+
+	}
+}
