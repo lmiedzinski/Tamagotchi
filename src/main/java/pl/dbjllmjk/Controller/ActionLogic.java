86d000ce4505a3ec410916f51da02740c1da778e
Łukasz Miedziński <luxorek@gmail.com>
2017-06-04 22:50:23 +0200

    Wyniesiono część logiki jako zewnętrzne metody, aby można było je testować

--- src/main/java/pl/dbjllmjk/Controller/ActionLogic.java
+++ src/main/java/pl/dbjllmjk/Controller/ActionLogic.java
@@ -1,7 +1,86 @@
 package pl.dbjllmjk.Controller;
 
+import pl.dbjllmjk.Model.Action;
+import pl.dbjllmjk.Model.Pet;
+import pl.dbjllmjk.Model.PetTransactionException;
+
+import java.time.LocalDateTime;
+
+
 /**
- * Created by lm on 04.06.2017.
+ * Klasa z logiką dotyczącą aktywności
  */
 public class ActionLogic {
+
+    /**
+     * Metoda symulująca karmienie zwierzaka
+     *
+     * @param p zwierzak
+     * @param a pokarm
+     * @return zaktualizowany zwierzak
+     * @throws PetTransactionException
+     */
+    public static Pet feedPet(Pet p, Action a) throws PetTransactionException {
+        if (p == null) {
+            throw new PetTransactionException("No such pet");
+        }
+        if (a == null) {
+            throw new PetTransactionException("No such action");
+        }
+        if (a.getValue() > p.getHunger())
+            throw new PetTransactionException("You can't feed pet more than " + p.getHunger());
+        else {
+            p.setHunger(p.getHunger() - a.getValue());
+            p.setLastFeedingDate(LocalDateTime.now());
+        }
+        return p;
+    }
+
+    /**
+     * Metoda symulująca zabawę ze zwierzakiem
+     *
+     * @param p zwierzak
+     * @param a rodzaj zabawy
+     * @return zaktualizowany zwierzak
+     * @throws PetTransactionException
+     */
+    public static Pet playWithPet(Pet p, Action a) throws PetTransactionException {
+        if (p == null) {
+            throw new PetTransactionException("No such pet");
+        }
+        if (a == null) {
+            throw new PetTransactionException("No such action");
+        }
+        if (a.getValue() > 10 - p.getHappiness())
+            throw new PetTransactionException("You can't play with pet more than " + (10 - p.getHappiness()));
+        else {
+            p.setHappiness(p.getHappiness() + a.getValue());
+            p.setLastActivityDate(LocalDateTime.now());
+        }
+        return p;
+    }
+
+    /**
+     * Metoda symulująca operację ze zwierzakiem
+     *
+     * @param p zwierzak
+     * @param a rodzaj operacji
+     * @return zaktualizowany zwierzak
+     * @throws PetTransactionException
+     */
+    public static Pet makeOperationOnPet(Pet p, Action a) throws PetTransactionException {
+        if (p == null) {
+            throw new PetTransactionException("No such pet");
+        }
+        if (a == null) {
+            throw new PetTransactionException("No such action");
+        }
+        if (a.getValue() > 10 - p.getHealth())
+            throw new PetTransactionException("You can't make operation on pet more than " + (10 - p.getHealth()));
+        else {
+            p.setHealth(p.getHealth() + a.getValue());
+            p.setLastOperationDate(LocalDateTime.now());
+        }
+        return p;
+    }
 }
