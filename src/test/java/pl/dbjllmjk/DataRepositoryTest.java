16891e1695ad60c1fc826d9d5cf7307a3a0af141
JakubKrysiak\Qbek <jotemka95@gmail.com>
2017-06-05 22:50:54 +0200

    [#21] Dodano dependency do mockito. Stworzona klasa do testow dla DataRepository

---
+++ src/test/java/pl/dbjllmjk/DataRepositoryTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.dbjllmjk;
+
+import com.healthmarketscience.jackcess.Database;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+/**
+ *
+ * @author Kubek
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DataRepositoryTest {
+    
+    
+    
+}
