fbeaaf1e8bdb8a568e75a6dadd143bceebc22789
lewanski195643 <mindf4ck3r@gmail.com>
2017-06-05 17:14:25 +0200

    [#26] Utworzono pakiety Utils oraz Exceptions.

---
+++ src/main/java/pl/dbjllmjk/Exceptions/NoSuchUserException.java
@@ -0,0 +1,27 @@
+package pl.dbjllmjk.Exceptions;
+
+/**
+ * {@link Exception} thrown when there is no user ({@link UserData}) or admin
+ * ({@link AdminData}) which contains of given login.
+ */
+public class NoSuchUserException extends Exception {
+
+    /**
+     * {@value}
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * {@link Exception#Exception()}
+     */
+    public NoSuchUserException() {
+        super();
+    }
+
+    /**
+     * {@link Exception#Exception(String)
+     */
+    public NoSuchUserException(String arg0) {
+        super(arg0);
+    }
+}
