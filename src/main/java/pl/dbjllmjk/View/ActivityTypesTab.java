c84d067f90c077db8085232e9c8c110f007b01d5
lewanski195643 <mindf4ck3r@gmail.com>
2017-06-05 18:06:55 +0200

    [#25] Zmieniono implementację niektórych metod w ActivityTypesTab

--- src/main/java/pl/dbjllmjk/View/ActivityTypesTab.java
+++ src/main/java/pl/dbjllmjk/View/ActivityTypesTab.java
@@ -3,8 +3,8 @@ package pl.dbjllmjk.View;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -41,9 +41,9 @@ public class ActivityTypesTab extends JPanel implements ActionListener, ListSele
         activityTypeList = new JList<>(activityTypes);
         activityTypeList.addListSelectionListener(this);
         connectionsModel = new DefaultListModel<>();
-        for (JCheckBox box : this.adminController.getActionWithTypeConnections(null)) {
-            connectionsModel.addElement(box);
-        }
+        this.adminController.getActionWithTypeConnections(null).entrySet().forEach((entry) -> {
+            connectionsModel.addElement(new JCheckBox(entry.getKey(), entry.getValue()));
+        });
         connectionsList = new CheckBoxList(connectionsModel);
         JPanel leftSide = new JPanel(new GridLayout(2, 1));
         JScrollPane activityScroll = new JScrollPane(activityTypeList);
@@ -102,9 +102,10 @@ public class ActivityTypesTab extends JPanel implements ActionListener, ListSele
             }
         }
         if (arg0.getSource() == this.saveChangesButton) {
-            List<JCheckBox> updated = new ArrayList<>();
+            Map<String,Boolean> updated = new HashMap<>();
             for (int i = 0; i < this.connectionsList.getModel().getSize(); i++) {
-                updated.add((JCheckBox) this.connectionsList.getModel().getElementAt(i));
+                JCheckBox box = (JCheckBox) this.connectionsList.getModel().getElementAt(i);
+                updated.put(box.getText(),box.isSelected());
             }
             try {
                 this.adminController.updateActionWithTypeConnections(updated, this.activityTypeList.getSelectedValue());
@@ -122,9 +123,9 @@ public class ActivityTypesTab extends JPanel implements ActionListener, ListSele
     public void valueChanged(ListSelectionEvent arg0) {
         Action selectedValue = this.activityTypeList.getSelectedValue();
         this.connectionsModel.clear();
-        for (JCheckBox box : this.adminController.getActionWithTypeConnections(selectedValue)) {
-            connectionsModel.addElement(box);
-        }
+        this.adminController.getActionWithTypeConnections(selectedValue).entrySet().forEach((entry) -> {
+            connectionsModel.addElement(new JCheckBox(entry.getKey(), entry.getValue()));
+        });
     }
 
 }
