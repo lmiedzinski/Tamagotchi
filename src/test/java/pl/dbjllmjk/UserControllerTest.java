7ddccfb215bb370f2443aab3cf2ded9162d0774f
Łukasz Miedziński <luxorek@gmail.com>
2017-06-06 19:50:19 +0200

    Poprawiono testy dla addPetByUser

--- src/test/java/pl/dbjllmjk/UserControllerTest.java
+++ src/test/java/pl/dbjllmjk/UserControllerTest.java
@@ -8,7 +8,8 @@ package pl.dbjllmjk;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
-import junit.framework.Assert;
+
+import org.junit.Assert;
 import org.junit.Test;
 import pl.dbjllmjk.Controller.AdminController;
 import pl.dbjllmjk.Controller.Controller;
@@ -20,10 +21,6 @@ import pl.dbjllmjk.Model.DataRepository;
 import pl.dbjllmjk.Model.Pet;
 import pl.dbjllmjk.Model.UserData;
 
-/**
- *
- * @author Damian
- */
 public class UserControllerTest {
 
     @Test
@@ -35,6 +32,7 @@ public class UserControllerTest {
         a.addAccount("test6", "testPassword", "testName", "testSurname", false);
         Optional<UserData> user = c.getDataRepository().getUsers().stream().filter(x -> x.getLogin().equals("test6")).findFirst();
         if (user.isPresent()) {
+            c = new Controller(1);
             c.afterLoginT(user.get());
             UserController uc = new UserController(c, 0);
             uc.addPet("testPet5", "dog");
@@ -44,12 +42,13 @@ public class UserControllerTest {
             if (optionalPet.isPresent()) {
                 result = true;
             }
-            c.getDataRepository().removePet(optionalPet.get());
-            c.getDataRepository().removeUser(user.get());
+            dr.removePet(optionalPet.get());
+            dr.removeUser(user.get());
             Assert.assertTrue(result);
         }
     }
-@Test(expected = PetTransactionException.class)
+
+    @Test(expected = PetTransactionException.class)
     public void addPetByUserTest2() throws PetTransactionException, NoSuchUserException, UnsupportedEncodingException, NoSuchAlgorithmException {
         Controller c = new Controller(1);
         Optional<AdminData> admin = c.getDataRepository().getAdmins().stream().filter(x -> x.getLogin().equals("admin")).findFirst();
@@ -58,14 +57,15 @@ public class UserControllerTest {
         a.addAccount("test10", "testPassword", "testName", "testSurname", false);
         Optional<UserData> user = c.getDataRepository().getUsers().stream().filter(x -> x.getLogin().equals("test10")).findFirst();
         if (user.isPresent()) {
+            c = new Controller(1);
             c.afterLoginT(user.get());
             UserController uc = new UserController(c, 0);
             c.getDataRepository().removeUser(user.get());
             uc.addPet("", "dog");
-            
         }
     }
-@Test(expected = PetTransactionException.class)
+
+    @Test(expected = PetTransactionException.class)
     public void addPetByUserTest3() throws PetTransactionException, NoSuchUserException, UnsupportedEncodingException, NoSuchAlgorithmException {
         Controller c = new Controller(1);
         Optional<AdminData> admin = c.getDataRepository().getAdmins().stream().filter(x -> x.getLogin().equals("admin")).findFirst();
@@ -74,11 +74,38 @@ public class UserControllerTest {
         a.addAccount("test10", "testPassword", "testName", "testSurname", false);
         Optional<UserData> user = c.getDataRepository().getUsers().stream().filter(x -> x.getLogin().equals("test10")).findFirst();
         if (user.isPresent()) {
+            c = new Controller(1);
             c.afterLoginT(user.get());
             UserController uc = new UserController(c, 0);
             c.getDataRepository().removeUser(user.get());
             uc.addPet("jaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaamnik", "dog");
-            
+
+        }
+    }
+
+    @Test
+    public void addPetByUserTest4() throws PetTransactionException, NoSuchUserException, UnsupportedEncodingException, NoSuchAlgorithmException {
+        Controller c = new Controller(1);
+        Optional<AdminData> admin = c.getDataRepository().getAdmins().stream().filter(x -> x.getLogin().equals("admin")).findFirst();
+        c.afterLoginT(admin.get());
+        AdminController a = new AdminController(c, null);
+        a.addAccount("test10", "testPassword", "testName", "testSurname", false);
+        Optional<UserData> user = c.getDataRepository().getUsers().stream().filter(x -> x.getLogin().equals("test10")).findFirst();
+        if (user.isPresent()) {
+            c = new Controller(1);
+            c.afterLoginT(user.get());
+            UserController uc = new UserController(c, 0);
+            uc.addPet("jaaaaamnik", "dog");
+            try {
+                uc.addPet("jaaaaamnik", "dog");
+            } catch (PetTransactionException e) {
+                Assert.assertTrue(true);
+            } finally {
+                DataRepository dr = new DataRepository();
+                Optional<Pet> petToDelete = c.getDataRepository().getPetsForUser(user.get()).stream().filter(x -> x.getName().equals("jaaaaamnik")).findFirst();
+                if (petToDelete.isPresent()) dr.removePet(petToDelete.get());
+                dr.removeUser(user.get());
+            }
         }
     }
 
